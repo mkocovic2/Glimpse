7f63354fb58c04b370fd314c32abcd74675dd5da
Michael Kocovic <michaelk2ny@gmail.com>
2025-04-18 16:16:58 -0400

    Added main JavaFX files, this will be the baseline of our project. It's a simple Hello World application, and we will build further upon this as time progresses.

---
+++ Glimpse/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.exodus.glimpse {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.exodus.glimpse to javafx.fxml;
+    exports com.exodus.glimpse;
+}
